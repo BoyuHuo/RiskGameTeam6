e2be0483ee9624a30e20619472da0054ba408196
boyu.huo.china@gmail.com <boyu.huo.china@gmail.com>
2019-02-19 18:05:23 -0500

    Junit example for Hao

--- src/RiskGame/test/TestMapReader.java
+++ src/RiskGame/test/TestMapReader.java
@@ -1,16 +1,20 @@
 package RiskGame.test;
+import RiskGame.model.entity.GameMap;
 import RiskGame.model.service.imp.MapManager;
 import org.junit.*;
+
+import static org.junit.Assert.*;
 
 public class TestMapReader {
     MapManager mapManager ;
     @Before
     public void setUp(){
-        mapManager = new MapManager();
+        mapManager=new MapManager();
     }
 
     @Test
-    public void testMapReader1(){
-        mapManager.LoadMap("");
+    public void testMapLoadNotNull(){
+        GameMap map=mapManager.LoadMap("D:\\1.Study\\Concordia\\SOEN6441 APP\\riskGameTeam6\\map\\2.map");
+        assertNotNull(map);
     }
 }
