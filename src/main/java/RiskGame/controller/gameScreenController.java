86387b0f24923d04a2f76eb07ca2856d8f44d655
Runsen <rinotrs@gmail.com>
2019-02-26 16:36:58 -0500

    Modify the game UI, add CSS file

--- src/main/java/RiskGame/controller/gameScreenController.java
+++ src/main/java/RiskGame/controller/gameScreenController.java
@@ -1,4 +1,34 @@
 package RiskGame.controller;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class gameScreenController {
+public class gameScreenController{
+
+        @FXML
+        private Button myCards;
+
+        @FXML
+        private void newButtonOnClicked() {
+                try {
+                        Parent anotherRoot = FXMLLoader.load(getClass().getResource("cardScreen.fxml"));
+                        Stage anotherStage = new Stage();
+                        anotherStage.setTitle("My cards");
+                        anotherStage.setScene(new Scene(anotherRoot, 600, 329));
+                        anotherStage.show();
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+        }
+
 }
