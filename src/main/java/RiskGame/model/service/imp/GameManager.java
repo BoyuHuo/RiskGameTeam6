6274f7bfef9b7dc3d29c55a2330b93aa685b9e54
boyu.huo.china@gmail.com <boyu.huo.china@gmail.com>
2019-02-27 14:41:10 -0500

    Update the game manager, and some refactoring

--- src/main/java/RiskGame/model/service/imp/GameManager.java
+++ src/main/java/RiskGame/model/service/imp/GameManager.java
@@ -1,6 +1,99 @@
 package RiskGame.model.service.imp;
 
+import RiskGame.model.entity.GameMap;
+import RiskGame.model.entity.Player;
 import RiskGame.model.service.IGameManager;
+import com.sun.scenario.effect.impl.prism.ps.PPSBlend_ADDPeer;
 
-public class GameManager implements IGameManager {
+import java.util.*;
+
+public class GameManager extends Observable implements IGameManager {
+
+    enum phase {STARTUP, REINFORCEMENTS, ATTACK, FORTIFICATION}
+
+    private Map<String, Player> players;
+    private Iterator playerIterator;
+    private Player activePlayer;
+    private GameMap map;
+    private phase gamePhase;
+
+    public void NewGame(Map<String, Player> players, GameMap map) {
+        this.players = players;
+        this.map = map;
+        start();
+    }
+
+    public void start() {
+        startUpPhase();
+
+    }
+
+    public void gameOver() {
+
+    }
+
+    public void startUpPhase() {
+        gamePhase = phase.STARTUP;
+        nextPlayer();
+
+    }
+
+    public void nextPlayer() {
+        if (playerIterator == null || !playerIterator.hasNext()) {
+            playerIterator = players.values().iterator();
+        }
+        activePlayer = (Player) playerIterator.next();
+    }
+
+    public GameMap getMap() {
+        return map;
+    }
+
+    public void setMap(GameMap map) {
+        this.map = map;
+    }
+
+    public void setPlayers(Map<String, Player> players) {
+        this.players = players;
+    }
+
+    public Map<String, Player> getPlayers() {
+        return this.players;
+    }
+
+    public void addPlayer(Player p){
+        if (players==null || players.isEmpty()){
+            players=new HashMap<>();
+        }
+        players.put(p.getName(),p);
+    }
+    public void removePlayer(Player p){
+        players.remove(p.getName());
+    }
+
+    public Player getActivePlayer() {
+        return activePlayer;
+    }
+
+    public void setActivePlayer(Player activePlayer) {
+        this.activePlayer = activePlayer;
+    }
+
+    public phase getGamePhase() {
+        return gamePhase;
+    }
+
+    public void setGamePhase(phase gamePhase) {
+        this.gamePhase = gamePhase;
+    }
+
+    public Iterator getPlayerIterator() {
+        return this.playerIterator;
+    }
+
+    public void setPlayerIterator(Iterator iterator) {
+        this.playerIterator = iterator;
+    }
+
+
 }
