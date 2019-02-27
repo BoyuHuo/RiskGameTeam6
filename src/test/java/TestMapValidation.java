42012cec28517b14d7b10aa7ffe8bc65c59ce968
Pisces_Hao <mh2923166@gmail.com>
2019-02-26 21:20:29 -0500

    already finished the 'TestMapValidation' and improved the four map test cases

--- src/test/java/TestMapValidation.java
+++ src/test/java/TestMapValidation.java
@@ -25,10 +25,64 @@ public class TestMapValidation {
     public void setUp() {
         mapManager = new MapManager();
     }
-
-///    @Test
-///    public void testMapIsValided() {
- ///       GameMap map = mapManager.LoadMap(getClass().getResource("/map/PekmonLand.map").getPath());
- ///       assertTrue(mapManager.IsValided(map));
+    /**
+     * Test case 1
+     * Purpose: testing the Validation of a loaded map
+     * Process:
+     * <ul>
+     *     <li>load a existed map</li>
+     *     <li>check if the map is valided</li>
+     * </ul>
+     *
+     */
+    @Test
+    public void testMapIsValided() {
+        GameMap map = mapManager.LoadMap(getClass().getResource("/map/PekmonLand.map").getPath());
+        assertTrue(mapManager.IsValided(map));
+    }
+    /**
+     * Test case 2
+     * Purpose: testing the Validation of a loaded map
+     * Process:
+     * <ul>
+     *     <li>load a test map(Some Continents of this map are not connected)</li>
+     *     <li>check if the map is not valided</li>
+     * </ul>
+     *
+     */
+    @Test
+    public void testMapWithDisconnectedContientsIsNotValided(){
+        GameMap map = mapManager.LoadMap(getClass().getResource("/map/PekwrongLandOne.map").getPath());
+        assertFalse(mapManager.IsValided(map));
+    }
+    /**
+     * Test case 3
+     * Purpose: testing the Validation of a loaded map
+     * Process:
+     * <ul>
+     *     <li>load a test map(The Territories of some continents are not connected)</li>
+     *     <li>check if the map is not valided</li>
+     * </ul>
+     *
+     */
+    @Test
+    public void testMapWithDisconnectedTerritoriesIsNotValided(){
+        GameMap map = mapManager.LoadMap(getClass().getResource("/map/PekwrongLandTwo.map").getPath());
+        assertFalse(mapManager.IsValided(map));
+    }
+    /**
+     * Test case 4
+     * Purpose: testing the Validation of a loaded map
+     * Process:
+     * <ul>
+     *     <li>load a test map(There are not only some disconnected Continents but also some disconnected Territories)</li>
+     *     <li>check if the map is not valided</li>
+     * </ul>
+     *
+     */
+    @Test
+    public void testMapWithMixedProblemsIsNotValided(){
+        GameMap map = mapManager.LoadMap(getClass().getResource("/map/PekwrongLandMix.map").getPath());
+        assertFalse(mapManager.IsValided(map));
     }
 }
