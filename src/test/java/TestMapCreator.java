a3db8c3e484cf9d98e976014f89b394f466daee1
Pisces_Hao <mh2923166@gmail.com>
2019-03-01 16:18:47 -0500

    Almost finished all the tests of the TestMapCreator

--- src/test/java/TestMapCreator.java
+++ src/test/java/TestMapCreator.java
@@ -93,5 +93,156 @@ public class TestMapCreator {
         assertEquals(2,result.getContinents().size());
     }
 
+    /**
+     * Test case 2
+     * Purpose: testing the function of creating a map
+     * Process:
+     * <ul>
+     *     <li>create a connected map, which contains 5 territory</li>
+     *     <li>3 continents which are also connected sub-graph</li>
+     *     <li>save into source folder as NewMap1.map</li>
+     *     <li>read the map file</li>
+     *     <li>check if the information is correct</li>
+     * </ul>
+     *
+     */
+    @Test
+    public void testCreateTwo() {
+        System.out.println("Test case 2: Test Map Create has been start...");
+        GameMap map = new GameMap();
+        map.setAuthor("Hao Ma");
+        map.setScroll("vertical");
+        map.setWarn("no");
+        map.setImage("null");
+        map.setWrap("no");
+        Territory f1= new Territory("Fire1",100,100);
+        Territory f2 = new Territory("Fire2",100,150);
+        Territory w1 = new Territory("Water1",200,150);
+        Territory w2= new Territory("Water2",200,200);
+        Territory i1= new Territory("Ice1",300,100);
+        f1.getNeighbors().put(f2.getName(),f2);
+        f2.getNeighbors().put(w1.getName(),w1);
+        w1.getNeighbors().put(w2.getName(),w2);
+        w2.getNeighbors().put(i1.getName(),i1);
+        i1.getNeighbors().put(f1.getName(),f1);
+        Continent c = new Continent("Canada",22);
+        Continent m = new Continent("Montrea",16);
+        Continent q = new Continent("Quebec",18);
+        f1.setContinent(c);
+        f2.setContinent(c);
+        w1.setContinent(m);
+        w2.setContinent(m);
+        i1.setContinent(q);
+        map.getContinents().put(c.getName(),c);
+        map.getContinents().put(m.getName(),m);
+        map.getContinents().put(q.getName(),q);
+        map.getTerritories().put(f1.getName(),f1);
+        map.getTerritories().put(f2.getName(),f2);
+        map.getTerritories().put(w1.getName(),w1);
+        map.getTerritories().put(w2.getName(),w2);
+        map.getTerritories().put(i1.getName(),i1);
 
+        mapManager.CreateMap(getClass().getResource("/map/").getPath()+"NewMap1.map", map);
+        GameMap result = mapManager.LoadMap(getClass().getResource("/map/").getPath()+"NewMap1.map");
+        assertEquals("Hao Ma",result.getAuthor());
+        assertEquals(3,result.getContinents().size());
+        assertEquals(5,result.getTerritories().size());
+        assertEquals(16,m.getCtrNum());
+        assertEquals(22,c.getCtrNum());
+    }
+
+    /**
+     * Test case 3
+     * Purpose: testing the function of creating a map
+     * Process:
+     * <ul>
+     *     <li>create a test map, which contains 4 territory</li>
+     *     <li>3 continents which have some disconnected relationship</li>
+     *     <li>save into source folder as NewMap1.map</li>
+     *     <li>read the map file</li>
+     *     <li>check if the information is correct</li>
+     * </ul>
+     *
+     */
+    @Test
+    public void testCreateThree() {
+        System.out.println("Test case 3: Test Map Create has been start...");
+        GameMap map = new GameMap();
+        map.setAuthor("Hao Ma");
+        map.setScroll("vertical");
+        map.setWarn("no");
+        map.setImage("null");
+        map.setWrap("no");
+        Territory w1= new Territory("Wind1",125,125);
+        Territory w2 = new Territory("Wind2",125,175);
+        Territory s1 = new Territory("Sun1",225,175);
+        Territory m1= new Territory("Moon1",325,125);
+        w1.getNeighbors().put(w2.getName(),w2);
+        s1.getNeighbors().put(m1.getName(),m1);
+        Continent a = new Continent("Apple",23);
+        Continent b = new Continent("Banana",12);
+        Continent p = new Continent("Peach",16);
+        w1.setContinent(a);
+        w2.setContinent(a);
+        s1.setContinent(b);
+        m1.setContinent(p);
+        map.getContinents().put(a.getName(),a);
+        map.getContinents().put(b.getName(),b);
+        map.getContinents().put(p.getName(),p);
+        map.getTerritories().put(w1.getName(),w1);
+        map.getTerritories().put(w2.getName(),w2);
+        map.getTerritories().put(s1.getName(),s1);
+        map.getTerritories().put(m1.getName(),m1);
+
+        mapManager.CreateMap(getClass().getResource("/map/").getPath()+"NewMap1.map", map);
+        GameMap result = mapManager.LoadMap(getClass().getResource("/map/").getPath()+"NewMap1.map");
+    }
+
+    /**
+     * Test case 4
+     * Purpose: testing the function of creating a map
+     * Process:
+     * <ul>
+     *     <li>create a test map, which contains 3 continents</li>
+     *     <li>4 territories which have some disconnected relationship</li>
+     *     <li>save into source folder as NewMap1.map</li>
+     *     <li>read the map file</li>
+     *     <li>check if the information is correct</li>
+     * </ul>
+     *
+     */
+    @Test
+    public void testCreateThree() {
+        System.out.println("Test case 4: Test Map Create has been start...");
+        GameMap map = new GameMap();
+        map.setAuthor("Hao Ma");
+        map.setScroll("vertical");
+        map.setWarn("no");
+        map.setImage("null");
+        map.setWrap("no");
+        Territory b1= new Territory("Basketball1",225,225);
+        Territory b2 = new Territory("Basketball2",225,275);
+        Territory f1 = new Territory("Football1",325,275);
+        Territory s1= new Territory("Swimming1",425,225);
+        b2.getNeighbors().put(f1.getName(),f1);
+        f1.getNeighbors().put(s1.getName(),s1);
+        s1.getNeighbors().put(b1.getName(),b1);
+        Continent r = new Continent("Red",16);
+        Continent y = new Continent("Yellow",19);
+        Continent d = new Continent("Darkness",18);
+        b1.setContinent(r);
+        b2.setContinent(r);
+        f1.setContinent(y);
+        s1.setContinent(d);
+        map.getContinents().put(r.getName(),r);
+        map.getContinents().put(y.getName(),y);
+        map.getContinents().put(d.getName(),d);
+        map.getTerritories().put(b1.getName(),b1);
+        map.getTerritories().put(b2.getName(),b2);
+        map.getTerritories().put(f1.getName(),f1);
+        map.getTerritories().put(s1.getName(),s1);
+
+        mapManager.CreateMap(getClass().getResource("/map/").getPath()+"NewMap1.map", map);
+        GameMap result = mapManager.LoadMap(getClass().getResource("/map/").getPath()+"NewMap1.map");
+    }
 }
