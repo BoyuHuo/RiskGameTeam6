229a81b776d2069cd5cf6c8e8cd5f5aef7830312
boyu.huo.china@gmail.com <boyu.huo.china@gmail.com>
2019-03-25 15:34:14 -0400

    attacking bug fixed

--- src/main/java/RiskGame/model/entity/Player.java
+++ src/main/java/RiskGame/model/entity/Player.java
@@ -305,7 +305,7 @@ public class Player {
             GameManager.getInstance().setMessage("[Attacker] " + this.getName() + "'s Total toll: " + resInt[0] + "\n");
             GameManager.getInstance().setMessage("[Defender] " + target.getName() + "'s Total toll:" + resInt[1] + "\n");
 
-            this.setArmies(armies - Integer.parseInt(resInt[0]));
+            source.setArmies(source.getArmies() - Integer.parseInt(resInt[0]));
             target.setArmies(target.getArmies() - Integer.parseInt(resInt[1]));
 
             if (target.getArmies() == 0) {
