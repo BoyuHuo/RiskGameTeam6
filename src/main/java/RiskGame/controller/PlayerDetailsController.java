1e7a718fb4cd6b24cbc5bcdc258758e65f9d9153
sudhanva2001 <sudhanva.2001@gmail.com>
2019-04-08 21:31:30 -0400

    Player Screen Changes, Tournament Screen changes, Edit PLayer changes.

--- src/main/java/RiskGame/controller/PlayerDetailsController.java
+++ src/main/java/RiskGame/controller/PlayerDetailsController.java
@@ -1,6 +1,8 @@
 package RiskGame.controller;
 
 import RiskGame.model.entity.Player;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -29,8 +31,19 @@ public class PlayerDetailsController implements Initializable {
 
     @FXML
     TextField txtPlayer1,txtPlayer2,txtPlayer3,txtPlayer4,txtPlayer5,txtPlayer6,txtPlayer7,txtPlayer8;
+
+    @FXML
+    TextField txtComp1,txtComp2,txtComp3,txtComp4,txtComp5,txtComp6,txtComp7,txtComp8;
+
+    @FXML
+    ChoiceBox<String> cbComp1,cbComp2,cbComp3,cbComp4,cbComp5,cbComp6,cbComp7,cbComp8;
+
     @FXML
     Label lblPlayer1,lblPlayer2,lblPlayer3,lblPlayer4,lblPlayer5,lblPlayer6,lblPlayer7,lblPlayer8;
+
+    @FXML
+    Label lblComp1,lblComp2,lblComp3,lblComp4,lblComp5,lblComp6,lblComp7,lblComp8;
+
     @FXML
     Button btnBack, btnSave;
 
@@ -41,6 +54,8 @@ public class PlayerDetailsController implements Initializable {
     private Scene newGameSceenScene;
     private File gameMapFile;
 
+    EditPlayerDetailsController editPlayer = new EditPlayerDetailsController();
+    private ObservableList playerBehaviour = FXCollections.observableArrayList();
     /**
      *<p>
      * This method implements Save Button to save player details.
@@ -60,33 +75,33 @@ public class PlayerDetailsController implements Initializable {
             case 2:
                     createPlayer(txtPlayer1.getText());
                     createPlayer(txtPlayer2.getText());
-                    playerName.add(0,txtPlayer1.getText());
-                    playerName.add(1,txtPlayer2.getText());
+                    playerName.add(txtPlayer1.getText());
+                    playerName.add(txtPlayer2.getText());
                     break;
 
-            case 3: playerName.add(0,txtPlayer1.getText());
-                    playerName.add(1,txtPlayer2.getText());
-                    playerName.add(2,txtPlayer3.getText());
+            case 3: playerName.add(txtPlayer1.getText());
+                    playerName.add(txtPlayer2.getText());
+                    playerName.add(txtPlayer3.getText());
                     createPlayer(txtPlayer1.getText());
                     createPlayer(txtPlayer2.getText());
                     createPlayer(txtPlayer3.getText());
                     break;
 
-            case 4: playerName.add(0,txtPlayer1.getText());
-                    playerName.add(1,txtPlayer1.getText());
-                    playerName.add(2,txtPlayer1.getText());
-                    playerName.add(3,txtPlayer1.getText());
+            case 4: playerName.add(txtPlayer1.getText());
+                    playerName.add(txtPlayer2.getText());
+                    playerName.add(txtPlayer3.getText());
+                    playerName.add(txtPlayer4.getText());
                     createPlayer(txtPlayer1.getText());
                     createPlayer(txtPlayer2.getText());
                     createPlayer(txtPlayer3.getText());
                     createPlayer(txtPlayer4.getText());
                     break;
 
-            case 5: playerName.add(0,txtPlayer1.getText());
-                    playerName.add(1,txtPlayer1.getText());
-                    playerName.add(2,txtPlayer1.getText());
-                    playerName.add(3,txtPlayer1.getText());
-                    playerName.add(4,txtPlayer1.getText());
+            case 5: playerName.add(txtPlayer1.getText());
+                    playerName.add(txtPlayer2.getText());
+                    playerName.add(txtPlayer3.getText());
+                    playerName.add(txtPlayer4.getText());
+                    playerName.add(txtPlayer5.getText());
                     createPlayer(txtPlayer1.getText());
                     createPlayer(txtPlayer2.getText());
                     createPlayer(txtPlayer3.getText());
@@ -94,12 +109,12 @@ public class PlayerDetailsController implements Initializable {
                     createPlayer(txtPlayer5.getText());
                     break;
 
-            case 6: playerName.add(0,txtPlayer1.getText());
-                    playerName.add(1,txtPlayer1.getText());
-                    playerName.add(2,txtPlayer1.getText());
-                    playerName.add(3,txtPlayer1.getText());
-                    playerName.add(4,txtPlayer1.getText());
-                    playerName.add(5,txtPlayer1.getText());
+            case 6: playerName.add(txtPlayer1.getText());
+                    playerName.add(txtPlayer2.getText());
+                    playerName.add(txtPlayer3.getText());
+                    playerName.add(txtPlayer4.getText());
+                    playerName.add(txtPlayer5.getText());
+                    playerName.add(txtPlayer6.getText());
                     createPlayer(txtPlayer1.getText());
                     createPlayer(txtPlayer2.getText());
                     createPlayer(txtPlayer3.getText());
@@ -108,13 +123,13 @@ public class PlayerDetailsController implements Initializable {
                     createPlayer(txtPlayer6.getText());
                 break;
 
-            case 7: playerName.add(0,txtPlayer1.getText());
-                    playerName.add(1,txtPlayer1.getText());
-                    playerName.add(2,txtPlayer1.getText());
-                    playerName.add(3,txtPlayer1.getText());
-                    playerName.add(4,txtPlayer1.getText());
-                    playerName.add(5,txtPlayer1.getText());
-                    playerName.add(6,txtPlayer1.getText());
+            case 7: playerName.add(txtPlayer1.getText());
+                    playerName.add(txtPlayer2.getText());
+                    playerName.add(txtPlayer3.getText());
+                    playerName.add(txtPlayer4.getText());
+                    playerName.add(txtPlayer5.getText());
+                    playerName.add(txtPlayer6.getText());
+                    playerName.add(txtPlayer7.getText());
                     createPlayer(txtPlayer1.getText());
                     createPlayer(txtPlayer2.getText());
                     createPlayer(txtPlayer3.getText());
@@ -124,14 +139,14 @@ public class PlayerDetailsController implements Initializable {
                     createPlayer(txtPlayer7.getText());
                     break;
 
-            case 8: playerName.add(0,txtPlayer1.getText());
-                    playerName.add(1,txtPlayer1.getText());
-                    playerName.add(2,txtPlayer1.getText());
-                    playerName.add(3,txtPlayer1.getText());
-                    playerName.add(4,txtPlayer1.getText());
-                    playerName.add(5,txtPlayer1.getText());
-                    playerName.add(6,txtPlayer1.getText());
-                    playerName.add(7,txtPlayer1.getText());
+            case 8: playerName.add(txtPlayer1.getText());
+                    playerName.add(txtPlayer2.getText());
+                    playerName.add(txtPlayer3.getText());
+                    playerName.add(txtPlayer4.getText());
+                    playerName.add(txtPlayer5.getText());
+                    playerName.add(txtPlayer6.getText());
+                    playerName.add(txtPlayer7.getText());
+                    playerName.add(txtPlayer8.getText());
                     createPlayer(txtPlayer1.getText());
                     createPlayer(txtPlayer2.getText());
                     createPlayer(txtPlayer3.getText());
@@ -141,6 +156,104 @@ public class PlayerDetailsController implements Initializable {
                     createPlayer(txtPlayer7.getText());
                     createPlayer(txtPlayer8.getText());
                     break;
+        }
+
+        switch (EditPlayerDetailsController.numberOfComputerPlayers)
+        {
+            case 1: playerName.add(txtComp1.getText());
+                createPlayer(txtComp1.getText());
+                break;
+
+            case 2:
+                createPlayer(txtComp1.getText());
+                createPlayer(txtComp2.getText());
+                playerName.add(txtComp1.getText());
+                playerName.add(txtComp2.getText());
+                break;
+
+            case 3: playerName.add(txtComp1.getText());
+                playerName.add(txtComp2.getText());
+                playerName.add(txtComp3.getText());
+                createPlayer(txtComp1.getText());
+                createPlayer(txtComp2.getText());
+                createPlayer(txtComp3.getText());
+                break;
+
+            case 4:
+                playerName.add(txtComp1.getText());
+                playerName.add(txtComp2.getText());
+                playerName.add(txtComp3.getText());
+                playerName.add(txtComp4.getText());
+                createPlayer(txtComp1.getText());
+                createPlayer(txtComp2.getText());
+                createPlayer(txtComp3.getText());
+                createPlayer(txtComp4.getText());
+                break;
+
+            case 5:
+                playerName.add(txtComp1.getText());
+                playerName.add(txtComp2.getText());
+                playerName.add(txtComp3.getText());
+                playerName.add(txtComp4.getText());
+                playerName.add(txtComp5.getText());
+                createPlayer(txtComp1.getText());
+                createPlayer(txtComp2.getText());
+                createPlayer(txtComp3.getText());
+                createPlayer(txtComp4.getText());
+                createPlayer(txtComp5.getText());
+                break;
+
+            case 6:
+                playerName.add(txtComp1.getText());
+                playerName.add(txtComp2.getText());
+                playerName.add(txtComp3.getText());
+                playerName.add(txtComp4.getText());
+                playerName.add(txtComp5.getText());
+                playerName.add(txtComp6.getText());
+
+                createPlayer(txtComp1.getText());
+                createPlayer(txtComp2.getText());
+                createPlayer(txtComp3.getText());
+                createPlayer(txtComp4.getText());
+                createPlayer(txtComp5.getText());
+                createPlayer(txtComp6.getText());
+                break;
+
+            case 7:
+                playerName.add(txtComp1.getText());
+                playerName.add(txtComp2.getText());
+                playerName.add(txtComp3.getText());
+                playerName.add(txtComp4.getText());
+                playerName.add(txtComp5.getText());
+                playerName.add(txtComp6.getText());
+                playerName.add(txtComp7.getText());
+                createPlayer(txtComp1.getText());
+                createPlayer(txtComp2.getText());
+                createPlayer(txtComp3.getText());
+                createPlayer(txtComp4.getText());
+                createPlayer(txtComp5.getText());
+                createPlayer(txtComp6.getText());
+                createPlayer(txtComp7.getText());
+                break;
+
+            case 8:
+                playerName.add(txtComp1.getText());
+                playerName.add(txtComp2.getText());
+                playerName.add(txtComp3.getText());
+                playerName.add(txtComp4.getText());
+                playerName.add(txtComp5.getText());
+                playerName.add(txtComp6.getText());
+                playerName.add(txtComp7.getText());
+                playerName.add(txtComp7.getText());
+                createPlayer(txtComp1.getText());
+                createPlayer(txtComp2.getText());
+                createPlayer(txtComp3.getText());
+                createPlayer(txtComp4.getText());
+                createPlayer(txtComp5.getText());
+                createPlayer(txtComp6.getText());
+                createPlayer(txtComp7.getText());
+                createPlayer(txtComp8.getText());
+                break;
         }
 
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -153,6 +266,21 @@ public class PlayerDetailsController implements Initializable {
         btnBack.setDisable(false);
 
         showGameScreen();
+    }
+    private void loadPlayerBehaviour()
+    {
+        playerBehaviour.removeAll(playerBehaviour);
+        playerBehaviour.addAll("Aggressive","Cheater","Benevolent","Random");
+        cbComp1.getItems().addAll(playerBehaviour);
+        cbComp2.getItems().addAll(playerBehaviour);
+        cbComp3.getItems().addAll(playerBehaviour);
+        cbComp4.getItems().addAll(playerBehaviour);
+        cbComp5.getItems().addAll(playerBehaviour);
+        cbComp6.getItems().addAll(playerBehaviour);
+        cbComp7.getItems().addAll(playerBehaviour);
+        cbComp8.getItems().addAll(playerBehaviour);
+
+
     }
 
     /**
@@ -199,7 +327,6 @@ public class PlayerDetailsController implements Initializable {
          playerList.put(text,player);
 
     }
-
     /**
      *<p>
      * This method generates random color.
@@ -244,7 +371,19 @@ public class PlayerDetailsController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+
         btnBack.setDisable(true);
+        loadPlayerBehaviour();
+        cbComp1.getSelectionModel().selectFirst();
+        cbComp2.getSelectionModel().selectFirst();
+        cbComp3.getSelectionModel().selectFirst();
+        cbComp4.getSelectionModel().selectFirst();
+        cbComp5.getSelectionModel().selectFirst();
+        cbComp6.getSelectionModel().selectFirst();
+        cbComp7.getSelectionModel().selectFirst();
+        cbComp8.getSelectionModel().selectFirst();
+
+
         switch (EditPlayerDetailsController.numberOfPlayers)
         {
             case 1: txtPlayer2.setDisable(true);
@@ -262,7 +401,7 @@ public class PlayerDetailsController implements Initializable {
                     lblPlayer6.setDisable(true);
                     lblPlayer7.setDisable(true);
                     lblPlayer8.setDisable(true);
-
+                    break;
 
 
             case 2:
@@ -279,7 +418,7 @@ public class PlayerDetailsController implements Initializable {
                 lblPlayer6.setDisable(true);
                 lblPlayer7.setDisable(true);
                 lblPlayer8.setDisable(true);
-
+                break;
             case 3:
                 txtPlayer4.setDisable(true);
                 txtPlayer5.setDisable(true);
@@ -292,7 +431,7 @@ public class PlayerDetailsController implements Initializable {
                 lblPlayer6.setDisable(true);
                 lblPlayer7.setDisable(true);
                 lblPlayer8.setDisable(true);
-
+                break;
             case 4:
                 txtPlayer5.setDisable(true);
                 txtPlayer6.setDisable(true);
@@ -303,7 +442,7 @@ public class PlayerDetailsController implements Initializable {
                 lblPlayer6.setDisable(true);
                 lblPlayer7.setDisable(true);
                 lblPlayer8.setDisable(true);
-
+                break;
             case 5:
                 txtPlayer6.setDisable(true);
                 txtPlayer7.setDisable(true);
@@ -312,18 +451,114 @@ public class PlayerDetailsController implements Initializable {
                 lblPlayer6.setDisable(true);
                 lblPlayer7.setDisable(true);
                 lblPlayer8.setDisable(true);
-
+                break;
             case 6:
                 txtPlayer7.setDisable(true);
                 txtPlayer8.setDisable(true);
 
                 lblPlayer7.setDisable(true);
                 lblPlayer8.setDisable(true);
-
+                break;
             case 7:
                 txtPlayer8.setDisable(true);
                 lblPlayer8.setDisable(true);
+                break;
+        }
 
+        switch (EditPlayerDetailsController.numberOfComputerPlayers)
+        {
+            case 2:
+                txtComp3.setDisable(true);
+                txtComp4.setDisable(true);
+                txtComp5.setDisable(true);
+                txtComp6.setDisable(true);
+                txtComp7.setDisable(true);
+                txtComp8.setDisable(true);
+
+                lblComp3.setDisable(true);
+                lblComp4.setDisable(true);
+                lblComp5.setDisable(true);
+                lblComp6.setDisable(true);
+                lblComp7.setDisable(true);
+                lblComp8.setDisable(true);
+
+                cbComp3.setDisable(true);
+                cbComp4.setDisable(true);
+                cbComp5.setDisable(true);
+                cbComp6.setDisable(true);
+                cbComp7.setDisable(true);
+                cbComp8.setDisable(true);
+
+                break;
+
+            case 3:
+                txtComp4.setDisable(true);
+                txtComp5.setDisable(true);
+                txtComp6.setDisable(true);
+                txtComp7.setDisable(true);
+                txtComp8.setDisable(true);
+
+                lblComp4.setDisable(true);
+                lblComp5.setDisable(true);
+                lblComp6.setDisable(true);
+                lblComp7.setDisable(true);
+                lblComp8.setDisable(true);
+
+                cbComp4.setDisable(true);
+                cbComp5.setDisable(true);
+                cbComp6.setDisable(true);
+                cbComp7.setDisable(true);
+                cbComp8.setDisable(true);
+
+                break;
+            case 4:
+                txtComp5.setDisable(true);
+                txtComp6.setDisable(true);
+                txtComp7.setDisable(true);
+                txtComp8.setDisable(true);
+
+                lblComp5.setDisable(true);
+                lblComp6.setDisable(true);
+                lblComp7.setDisable(true);
+                lblComp8.setDisable(true);
+
+                cbComp5.setDisable(true);
+                cbComp6.setDisable(true);
+                cbComp7.setDisable(true);
+                cbComp8.setDisable(true);
+                break;
+
+            case 5:
+                txtComp6.setDisable(true);
+                txtComp7.setDisable(true);
+                txtComp8.setDisable(true);
+
+                lblComp6.setDisable(true);
+                lblComp7.setDisable(true);
+                lblComp8.setDisable(true);
+
+                cbComp6.setDisable(true);
+                cbComp7.setDisable(true);
+                cbComp8.setDisable(true);
+                break;
+
+            case 6:
+                txtComp7.setDisable(true);
+                txtComp8.setDisable(true);
+
+                lblComp7.setDisable(true);
+                lblComp8.setDisable(true);
+
+                cbComp7.setDisable(true);
+                cbComp8.setDisable(true);
+                break;
+
+            case 7:
+                txtComp8.setDisable(true);
+                lblComp8.setDisable(true);
+
+                cbComp8.setDisable(true);
+                break;
         }
     }
 
