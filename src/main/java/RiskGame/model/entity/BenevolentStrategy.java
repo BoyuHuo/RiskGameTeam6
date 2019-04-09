b763cd41782ff46bcc325379888189264b6e6783
boyu.huo.china@gmail.com <boyu.huo.china@gmail.com>
2019-04-08 23:39:28 -0400

    sartup phase auto fixed

--- src/main/java/RiskGame/model/entity/BenevolentStrategy.java
+++ src/main/java/RiskGame/model/entity/BenevolentStrategy.java
@@ -4,6 +4,7 @@ import RiskGame.model.service.RiskUtil;
 import RiskGame.model.service.imp.GameManager;
 
 import java.util.Map;
+import java.util.Random;
 
 public class BenevolentStrategy implements Strategy {
     @Override
@@ -57,6 +58,21 @@ public class BenevolentStrategy implements Strategy {
             }
         }
 
+        return true;
+    }
+
+    @Override
+    public boolean startup(int movementTime) {
+         Random random = new Random();
+        Map<String, Territory> territories = RiskUtil.getAllTerritoryFromPlayer(GameManager.getInstance().getActivePlayer());
+        String[] keys = territories.keySet().toArray(new String[0]);
+        while (GameManager.getInstance().getActivePlayer().getArmies() > 0) {
+            GameManager.getInstance().getMap().getTerritories().get(keys[random.nextInt(keys.length)]).increaseArmies(GameManager.getInstance().getActivePlayer());
+            RiskUtil.delay(movementTime / 2);
+        }
+
+        RiskUtil.delay(movementTime * 2);
+        GameManager.getInstance().nextRound();
         return true;
     }
 
